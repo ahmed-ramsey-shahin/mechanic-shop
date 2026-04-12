20faf5ec114062407010143fa74a6c48f038e760
Ahmed Ramsey <ahmed.ramsey.shahin@yandex.com>
2026-04-12 10:32:45 +0200

    Added MechanicShop.Domain.Common.Constants for domain layer constants

---
+++ src/MechanicShop.Domain/Common/Constants/MechanicShopConstants.cs
@@ -0,0 +1,7 @@
+namespace MechanicShop.Domain.Common.Constants;
+
+internal static class MechanicShopConstants
+{
+    public const decimal TaxRate = 0.15m;
+    public const string SystemUser = "System";
+}
