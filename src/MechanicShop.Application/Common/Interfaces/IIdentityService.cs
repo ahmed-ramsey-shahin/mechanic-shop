83a5ffcb51c242d75719c03df97e5baca68f8e89
Ahmed Ramsey <ahmed.ramsey.shahin@yandex.com>
2026-04-17 01:09:59 +0200

    Added IIdentityService to handle identity services Added ITokenProvider to provide tokens when needed Added IUser to mark entity as user

---
+++ src/MechanicShop.Application/Common/Interfaces/IIdentityService.cs
@@ -0,0 +1,13 @@
+using MechanicShop.Application.Features.Identity.Dtos;
+using MechanicShop.Domain.Common.Results;
+
+namespace MechanicShop.Application.Common.Interfaces;
+
+public interface IIdentityService
+{
+    Task<bool> IsInRoleAsync(Guid userId, string role, CancellationToken cancellationToken);
+    Task<bool> AuthorizeAsync(Guid userId, string? policyName, CancellationToken cancellationToken);
+    Task<Result<AppUserDto>> AuthenticateAsync(string email, string password, CancellationToken cancellationToken);
+    Task<Result<AppUserDto>> GetUserByIdAsync(Guid userId, CancellationToken cancellationToken);
+    Task<string?> GetUserNameAsync(Guid userId, CancellationToken cancellationToken);
+}
---
+++ src/MechanicShop.Application/Common/Interfaces/ITokenProvider.cs
@@ -0,0 +1,12 @@
+using System.Security.Claims;
+using MechanicShop.Application.Features.Identity;
+using MechanicShop.Application.Features.Identity.Dtos;
+using MechanicShop.Domain.Common.Results;
+
+namespace MechanicShop.Application.Common.Interfaces;
+
+public interface ITokenProvider
+{
+    Task<Result<TokenResponse>> GenerateJwtTokenAsync(AppUserDto user, CancellationToken cancellationToken);
+    ClaimsPrincipal? GetPrincipalFromExpiredToken(string token);
+}
