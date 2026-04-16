b1f55b0a6935891658bfae878027098ee5f6d2b4
Ahmed Ramsey <ahmed.ramsey.shahin@yandex.com>
2026-04-17 01:38:47 +0200

    Added UnhandledExceptionBehavior

---
+++ src/MechanicShop.Application/Common/Behaviors/UnhandledExceptionBehavior.cs
@@ -0,0 +1,23 @@
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace MechanicShop.Application.Common.Behaviors;
+
+public class UnhandledExceptionBehavior<TRequest, TResponse>(ILogger<TRequest> logger) : IPipelineBehavior<TRequest, TResponse> where TRequest : notnull
+{
+    private readonly ILogger<TRequest> _logger = logger;
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        try
+        {
+            return await next(cancellationToken);
+        }
+        catch(Exception ex)
+        {
+            var requestName = typeof(TRequest).Name;
+            _logger.LogError(ex, "Request: Unhandled exception for request {Name} {@Request}", requestName, request);
+            throw;
+        }
+    }
+}
