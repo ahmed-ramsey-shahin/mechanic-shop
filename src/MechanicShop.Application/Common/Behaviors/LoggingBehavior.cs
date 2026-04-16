1563b1bd627f59ea2d3b0e5245d0cd3f5bd1f5c3
Ahmed Ramsey <ahmed.ramsey.shahin@yandex.com>
2026-04-17 01:11:14 +0200

    Added a LoggingBehavior

---
+++ src/MechanicShop.Application/Common/Behaviors/LoggingBehavior.cs
@@ -0,0 +1,24 @@
+using MechanicShop.Application.Common.Interfaces;
+using MediatR.Pipeline;
+using Microsoft.Extensions.Logging;
+
+namespace MechanicShop.Application.Common.Behaviors;
+
+public class LoggingBehavior<TRequest>(ILogger<TRequest> logger, IUser user, IIdentityService identityService) : IRequestPreProcessor<TRequest> where TRequest : notnull
+{
+    private readonly ILogger<TRequest> _logger = logger;
+    private readonly IUser _user = user;
+    private readonly IIdentityService _identityService = identityService;
+
+    public async Task Process(TRequest request, CancellationToken cancellationToken)
+    {
+        var requestName = typeof(TRequest).Name;
+        var userId = _user.Id;
+        string? userName = string.Empty;
+        if (userId != Guid.Empty)
+        {
+            userName = await _identityService.GetUserNameAsync(userId, cancellationToken);
+        }
+        _logger.LogInformation("Request: {Name} {UserId} {Username} {@Request}", requestName, userId, userName, request);
+    }
+}
